7d563c27bfe36488e58ebd4672f6bce64bf8cea8
malBaskin <mebaskin@email.sc.edu>
2018-03-21 15:35:46 -0400

    update 1

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@bduj0j%*%d-4f)wyne%m@utb=4bkk-)oj+#hw-hoy+yzdph(4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
